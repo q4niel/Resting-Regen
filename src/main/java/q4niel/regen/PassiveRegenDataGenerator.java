d90dee3ed8df39d0276804811df305525d161d79
q4niel <github.obscure938@passmail.net>
2024-11-28 19:33:43 +0100

    Fabric Mod Template

---
+++ src/main/java/q4niel/regen/PassiveRegenDataGenerator.java
@@ -0,0 +1,9 @@
+package q4niel.regen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PassiveRegenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
+}
