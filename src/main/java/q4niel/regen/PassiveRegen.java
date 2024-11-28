1ca3a62dbc0f8e9530e13bf27599d663824258c0
q4niel <github.obscure938@passmail.net>
2024-11-28 20:10:00 +0100

    gamerule 'naturalRegeneration' set to 'false' by default

--- src/main/java/q4niel/regen/PassiveRegen.java
+++ src/main/java/q4niel/regen/PassiveRegen.java
@@ -2,6 +2,11 @@ package q4niel.regen;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,5 +15,10 @@ public class PassiveRegen implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {}
+	public void onInitialize() {
+		ServerWorldEvents.LOAD.register((server, world) -> {
+			GameRules.BooleanRule rule = world.getGameRules().get(GameRules.NATURAL_REGENERATION);
+			rule.set(false, world.getServer());
+		});
+	}
 }
