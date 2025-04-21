c655c6179c80a4026d59790b093643400e10f68a
q4niel <120566282+q4niel@users.noreply.github.com>
2025-04-21 14:25:05 +0200

    [kt-rework-of-0.0.1] (#1)
    * player now heals when out of combat, 1hp per 100tick
    * added readme

--- src/main/kotlin/dev/q4niel/resting_regen/RestingRegen.kt
+++ src/main/kotlin/dev/q4niel/resting_regen/RestingRegen.kt
@@ -1,6 +1,11 @@
 package dev.q4niel.resting_regen
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.world.GameRules
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -10,5 +15,34 @@ object RestingRegen: ModInitializer {
 	fun print(value: String): Unit = _logger.info(value);
     private val _logger: Logger = LoggerFactory.getLogger(MOD_ID)
 
-	override fun onInitialize() {}
+	fun serverExec(runnable: Runnable): Unit? = _server?.execute(runnable);
+	private var _server: MinecraftServer? = null;
+
+	fun getHealTickerLimit(): Int = _healTickerLimit;
+	private val _healTickerLimit: Int = 100;
+
+	fun getHorizontalBlockDistance(): Double = _horizontalBlockDistance;
+	private val _horizontalBlockDistance: Double = 20.0;
+
+	fun getVerticalBlockDistance(): Double = _verticalBlockDistance;
+	private val _verticalBlockDistance: Double = 10.0;
+
+	override fun onInitialize() {
+		ServerWorldEvents.LOAD.register {
+			server: MinecraftServer,
+			world: ServerWorld
+			->
+			world
+				.gameRules
+				.get(GameRules.NATURAL_REGENERATION)
+				.set(false, world.server)
+			;
+		};
+
+		ServerLifecycleEvents.SERVER_STARTED.register {
+			server: MinecraftServer
+			->
+			_server = server;
+		};
+	}
 }
