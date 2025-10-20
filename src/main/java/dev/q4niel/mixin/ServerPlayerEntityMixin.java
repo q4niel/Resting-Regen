1f0ca3060c843608c9cfb6c8e6ce0f17cbe8c521
q4niel <github.obscure938@passmail.net>
2025-10-20 13:30:55 +0200

    reimplementation of entire mod

---
+++ src/main/java/dev/q4niel/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,53 @@
+package dev.q4niel.mixin;
+
+import dev.q4niel.RestingRegen;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(ServerPlayerEntity.class)
+public class ServerPlayerEntityMixin {
+    private ServerPlayerEntity _self = (ServerPlayerEntity)(Object)this;
+    private int _healTicker = 0;
+
+    @Inject(method = "tick()V", at = @At("HEAD"))
+    void tick(CallbackInfo ci) {
+        if (_self.getHealth() == _self.getMaxHealth() || _hostilesNearby()) {
+            _healTicker = 0;
+            return;
+        }
+
+        if (_healTicker++ < RestingRegen.INSTANCE.getHealTickerLimit()) return;
+
+        _healTicker = 0;
+        _self.heal(1);
+    }
+
+    private boolean _hostilesNearby() {
+        Vec3d pos = _self.getEntityPos();
+        double y = RestingRegen.INSTANCE.getVerticalBlockDistance();
+        double xz = RestingRegen.INSTANCE.getHorizontalBlockDistance();
+
+        List<HostileEntity> list = _self.getEntityWorld().getEntitiesByClass (
+                HostileEntity.class,
+                new Box (
+                    pos.getX() - xz,
+                    pos.getY() - y,
+                    pos.getZ() - xz,
+                    pos.getX() + xz,
+                    pos.getY() + y,
+                    pos.getZ() + xz
+                ),
+                entity -> entity.isAngryAt(_self.getEntityWorld(), _self)
+        );
+
+        return !list.isEmpty();
+    }
+}
--- src/main/kotlin/dev/q4niel/RestingRegen.kt
+++ src/main/kotlin/dev/q4niel/RestingRegen.kt
@@ -1,6 +1,8 @@
 package dev.q4niel
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents
+import net.minecraft.world.GameRules
 import org.slf4j.LoggerFactory
 
 object RestingRegen : ModInitializer {
@@ -9,5 +11,13 @@ object RestingRegen : ModInitializer {
     private val _logger_ = LoggerFactory.getLogger(modID_);
     fun print(string: String): Unit = _logger_.info(string);
 
-	override fun onInitialize() {}
+    val healTickerLimit: Int = 100;
+    val horizontalBlockDistance: Double = 20.0;
+    val verticalBlockDistance: Double = 10.0;
+
+	override fun onInitialize() {
+        ServerWorldEvents.LOAD.register { server, world ->
+            world.gameRules.get(GameRules.NATURAL_REGENERATION).set(false, world.server);
+        }
+    }
 }
